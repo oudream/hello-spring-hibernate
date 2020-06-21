44e07fbe0fb701fab7e434b53b46583752b2ba51
oudream <oudream@live.com>
2020-06-21 17:50:18 +0800

    atguigu

---
+++ man/hibernate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+# https://docs.jboss.org/hibernate/orm/5.4/javadocs/
+# https://hibernate.org/orm/documentation/5.4/
+
+# https://hibernate.org/
+# https://github.com/hibernate
+
+# envers
+# https://docs.jboss.org/envers/docs/
+# https://www.cnblogs.com/pypua/p/10315015.html
+
+
